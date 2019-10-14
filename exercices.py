7fd786bb12364b7dc4069a9cb478516c7747e17e
nicos <nicolas.turck@gmail.com>
2019-10-14 21:08:09 +0200

    first commit

---
+++ exercices.py
@@ -0,0 +1,7 @@
+#exercice 1 : hello world
+print("exercice 1:Hello world\n")
+
+print("Hello World")#display Hello World
+
+linux = "Hello World" #i declare a variable
+print(linux)#print value of Linux
