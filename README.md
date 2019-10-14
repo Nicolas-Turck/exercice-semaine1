6c9ec1b09597ab6526a50c420fb300c409c91f69
nicos <nicolas.turck@gmail.com>
2019-10-14 21:59:03 +0200

    write in README.md  and adding exercise 2 in exercise.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # exercises of one week
 
 exercise 1: "Hello World"
+
+exercice 2 "calculs divers"
--- exercices.py
+++ exercices.py
@@ -5,3 +5,19 @@ print("Hello World")#display Hello World
 
 linux = "Hello World" #i declare a variable
 print(linux)#print value of Linux
+
+#exercise 2 : calculs divers
+print("\nexercise 2: calculs divers\n")
+
+print(3*3)#display result
+a = 12
+b = 0
+try:
+    a/b
+    print("result")
+except:
+    print("error")
+
+print(4+9+78)#display result
+print(12-7)#display result
+print(5e4) #display result
